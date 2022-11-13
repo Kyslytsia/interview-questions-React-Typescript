327a900fd671255c255b33cd151577e2dbdaac10
Denys <kislica779@gmail.com>
2022-11-13 12:21:43 +0200

    interview-questions

--- src/questions.js
+++ src/questions.js
@@ -54,16 +54,146 @@ let questions = [
       "Можете ли вы объяснить, что делают Function.call и Function.apply? В чем заметная разница между ними?",
     status: false,
   },
+  {
+    question: "Объясните Function.prototype.bind.",
+    status: false,
+  },
+  {
+    question:
+      "В чем разница между обнаружением функций, выводом функций и использованием строки UA?",
+    status: false,
+  },
+  {
+    question: "Объясните hoisting.",
+    status: false,
+  },
+  {
+    question: "Опишите всплытие событий.",
+    status: false,
+  },
+  {
+    question: "Опишите захват событий.",
+    status: false,
+  },
+  {
+    question: "Опишите захват событий.",
+    status: false,
+  },
+  {
+    question: "В чем разница между «атрибутом» и «свойством»?",
+    status: false,
+  },
+  {
+    question:
+      "Каковы плюсы и минусы расширения встроенных объектов JavaScript?",
+    status: false,
+  },
+  {
+    question: "What is the difference between == and ===?",
+    status: false,
+  },
+  {
+    question: "Объясните Same-origin policy в отношении JavaScript.",
+    status: false,
+  },
+  {
+    question:
+      "Почему он называется Тернарным оператором, что означает слово «Тернарный»?",
+    status: false,
+  },
+  {
+    question:
+      "Что такое строгий режим? Каковы некоторые преимущества / недостатки его использования?",
+    status: false,
+  },
+  {
+    question:
+      "Каковы некоторые преимущества/недостатки написания кода JavaScript на языке, который компилируется в JavaScript?",
+    status: false,
+  },
+  {
+    question:
+      "Какие инструменты и методы вы используете для отладки кода JavaScript?",
+    status: false,
+  },
+  {
+    question:
+      "Объясните разницу между изменяемым и неизменяемым объектом. Что является примером неизменяемого объекта в JavaScript? Каковы плюсы и минусы неизменности? Как добиться неизменности в собственном коде?",
+    status: false,
+  },
+  {
+    question: "Объясните разницу между синхронными и асинхронными функциями.",
+    status: false,
+  },
+  {
+    question:
+      "Что такое цикл событий? В чем разница между стеком вызовов и очередью задач?",
+    status: false,
+  },
+  {
+    question:
+      "В чем разница между переменными, созданными с помощью let, var или const?",
+    status: false,
+  },
+  {
+    question: "В чем разница между классом ES6 и конструкторами функций ES5?",
+    status: false,
+  },
+  {
+    question:
+      "Можете ли вы предложить вариант использования нового синтаксиса arrow function =>? Чем этот новый синтаксис отличается от других функций?",
+    status: false,
+  },
+  {
+    question:
+      "В чем преимущество использования синтаксиса стрелки для метода в конструкторе?",
+    status: false,
+  },
+  {
+    question: "Как определяется функция высшего порядка?",
+    status: false,
+  },
+  {
+    question:
+      "Можете ли вы привести пример деструктуризации объекта или массива?",
+    status: false,
+  },
+  {
+    question:
+      "Можете ли вы привести пример создания строки с помощью шаблонных литералов ES6?",
+    status: false,
+  },
+  {
+    question:
+      "Можете ли вы привести пример curry function и почему этот синтаксис дает преимущество?",
+    status: false,
+  },
+  {
+    question:
+      "Каковы преимущества использования синтаксиса spread и чем он отличается от синтаксиса rest?",
+    status: false,
+  },
+  {
+    question: "Как вы можете разделить код между файлами?",
+    status: false,
+  },
+  {
+    question: "Почему вы можете захотеть создать статические члены класса?",
+    status: false,
+  },
+  {
+    question: "В чем разница между циклами while и do-while в JavaScript?",
+    status: false,
+  },
+  {
+    question: "Что такое promise? Где и как вы бы использовали promise?",
+    status: false,
+  },
+  {
+    question:
+      "Обсудите, как вы можете использовать принципы объектно-ориентированного программирования при кодировании с помощью JavaScript.",
+    status: false,
+  },
 ];
 
 export default questions;
-
-// function randomQuestions(arr) {
-//   let random = Math.floor(Math.random() * arr.length);
-
-//   return arr[random];
-// }
-
-// let randomQuestsion = randomQuestions(questions);
-
-// export default randomQuestsion;
